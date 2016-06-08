ebea6c3aa1563fec3e0e1405eda0a0106b94aa60
Antti Härkönen <antth@hotmail.fi>
2016-06-08 15:49:49 +0300

    Kernel density surface

---
+++ KDESurface.py
@@ -0,0 +1,14 @@
+import kernel_functions as kf
+
+all_kernel_funcs = {
+    'distance_decay': kf.distance_decay
+}
+
+
+class KDESurface:
+    def __init__(self, data, kernel, kernel_param):
+        self._data = data
+        self._kernel = all_kernel_funcs[kernel]
+        self._param = kernel_param
+
+        # TODO matriisit
