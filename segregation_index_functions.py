301d2e41610582b2b9644de465e7c4126143a35b
Antti Härkönen <antth@hotmail.fi>
2016-06-10 12:30:01 +0300

    Spatial exporure index + km index

--- segregation_index_functions.py
+++ segregation_index_functions.py
@@ -16,22 +16,24 @@ def km(host: np.ndarray, other: np.ndarray):
     Karmel-MacLachlan index of segregation. Needs two matrices of the same size.
     :param host: host group
     :param other: other group
-    :return:
+    :return: km index
     """
+    index = 0
     pop = host + other
     total = pop.sum()
     p_n = pop / total
-    index = 0
+    y_dim, x_dim = host.shape
 
     for g in host, other:
-        y_dim, x_dim = g.shape
         index_g = 0
         p_g = g.sum() / total
+
         for i in range(y_dim):
             for j in range(x_dim):
-                p_gn = g[i][j] / total
-                index_gn = p_n[i][j] * abs(p_gn / p_g * p_n[i][j] - 1)
+                p_gn = np.nan_to_num(g[i][j] / total)
+                index_gn = p_n[i][j] * abs(np.nan_to_num(p_gn / (p_g * p_n[i][j]) - 1))
                 index_g += index_gn
+
         index += index_g * p_g
 
     return index
@@ -42,9 +44,19 @@ def hpg(host: np.ndarray, other: np.ndarray):
     Spatial exposure index. Needs two matrices of the same size.
     :param host: host group
     :param other: other group
-    :return:
+    :return: spatial exposure index
     """
-    pass
+    index = 0
+    t_n = host + other
+    t_h = host.sum()
+    y_dim, x_dim = host.shape
+
+    for i in range(y_dim):
+        for j in range(x_dim):
+            p_gn = np.nan_to_num(other[i][j] / t_n[i][j])
+            index += p_gn * host[i][j] / t_h
+
+    return index
 
 
 def gini(host: np.ndarray, other: np.ndarray):
@@ -52,13 +64,17 @@ def gini(host: np.ndarray, other: np.ndarray):
     Spatial Gini index. Needs two matrices of the same size.
     :param host: host group
     :param other: other group
-    :return:
+    :return: spatial gini index
     """
     pop = host + other
     p = np.nan_to_num(host / pop).ravel().tolist()
     lorenz = cum_sum(p)
-    m = sum(p) / len([p[i] for i in range(len(p)) if p[i] > 0])
+
+    m = sum(p) / len(
+        [p[i] for i in range(len(p)) if p[i] > 0])
+
     equal = [m * (i + 1) for i in range(len(lorenz))]
+
     return (sum(equal) - sum(lorenz)) / sum(equal)
 
 
@@ -83,12 +99,10 @@ def cum_sum(x):
 
 def main():
     h, o = np.random.rand(10, 12), np.random.rand(10, 12)
-    print(h.sum())
-    print(o.sum())
-    print(host_share(h, o))
-    print(km(h, o))
-    print(hpg(h, o))
-    print(gini(h, o))
+    print("host share: ", host_share(h, o))
+    print("km: ", km(h, o))
+    print("hpg: ", hpg(h, o))
+    print("gini: ", gini(h, o))
 
 
 if __name__ == '__main__':
