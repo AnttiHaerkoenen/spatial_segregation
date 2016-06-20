4e3b5c6420a283ee81cfa732365cae2be4848682
Antti Härkönen <antth@hotmail.fi>
2016-06-20 09:50:12 +0300

    rename

---
+++ segregation_analysis.py
@@ -0,0 +1,47 @@
+import data
+import pandas as pd
+import os
+import pysal
+
+DATA_FILE = 'data'
+
+
+class SegregationAnalysis:
+    def __init__(self, data, cell_sizes, kernel='distance_decay', **kernel_param):
+        self.data = data
+        # TODO kernels
+        # TODO cells
+
+
+########################################################################################################################
+
+
+def main():
+    os.chdir(DATA_FILE)
+    v80 = data.aggregate_sum(data.reform(pd.read_csv('1880.csv', sep='\t')))
+    v00 = data.aggregate_sum(data.reform(pd.read_csv('1900.csv', sep='\t')))
+    v20 = data.aggregate_sum(data.reform(pd.read_csv('1920.csv', sep='\t')))
+
+    pop_data = {
+        1880: v80,
+        1900: v00,
+        1920: v20
+    }
+
+    point_shp = pysal.open("points.shp")
+    point_db = pysal.open("points.dbf", 'r')
+
+    point_data = []
+    for i in range(len(point_shp)):
+        point_data.append([point_db[i][0][1], point_shp[i][0], point_shp[i][1]])
+
+    cells = [i for i in range(15, 61, 15)]
+    bws = [i for i in range(2, 6)]
+
+    data = data.Data(pop_data[1880], point_data, host_group='lutheran', other_group='orthodox', year=1880)
+
+    analysis = SegregationAnalysis(data=data, cell_sizes=cells, bandwidths=bws)
+
+
+if __name__ == "__main__":
+    main()
---
+++ segregation_indices.py
@@ -0,0 +1,43 @@
+import segregation_index_functions as sif
+
+all_index_functions = {
+    'host_share': sif.host_share,
+    'gini': sif.gini,
+    'km': sif.km,
+    'hpg': sif.hpg
+}
+
+
+class SegregationIndices:
+    def __init__(self, kde_surface, index_functions="all"):
+        if index_functions.lower() == "all":
+            self.index_functions = all_index_functions
+        else:
+            self.index_functions = {}
+            for key in index_functions:
+                self.index_functions[key] = all_index_functions[key]
+
+        self.data = kde_surface
+
+    def __str__(self):
+        string = ['Indices:']
+
+        for key in self.indices:
+            string.append('{0:10} {1}'.format(key, self.indices[key]))
+
+        return '\n'.join(string)
+
+    @property
+    def indices(self):
+        indices = {key: function(self.data) for key, function in self.index_functions}
+        return indices
+
+########################################################################################################################
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
