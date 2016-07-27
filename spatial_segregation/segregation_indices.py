a7b2e89840717b9a4252cd72293db78bd5e329ed
Antti Härkönen <antth@hotmail.fi>
2016-07-27 12:59:19 +0300

    KM testing

--- spatial_segregation/segregation_indices.py
+++ spatial_segregation/segregation_indices.py
@@ -8,6 +8,7 @@ def km(p_gn):
     :param p_gn: 2-dim array with groups as columns
     :return: km index
     """
+    p_gn /= p_gn.sum()
     p_g, p_n = np.broadcast_arrays(p_gn.sum(axis=0, keepdims=True),
                                    p_gn.sum(axis=1, keepdims=True))
 
--- spatial_segregation/tests/test_indices.py
+++ spatial_segregation/tests/test_indices.py
@@ -6,8 +6,8 @@ import spatial_segregation.segregation_indices as si
 
 rand = np.random.rand(1, 50)
 zeros = np.zeros((1, 50))
-data_a = np.tile(rand, (2, 2)).T
-data_b = np.hstack((np.vstack((rand, zeros)), np.vstack((zeros, rand))))
+data_a = np.hstack((np.vstack((rand, zeros)), np.vstack((zeros, rand)))).T
+data_b = np.tile(rand, (2, 2)).T
 
 
 class TestKM(unittest.TestCase):
@@ -15,10 +15,10 @@ class TestKM(unittest.TestCase):
         self.assertEqual(si.km(data_a), si.km(data_a))
 
     def test_km_total_segregation(self):
-        self.assertAlmostEqual(si.km(data_b), 1)
+        self.assertAlmostEqual(si.km(data_a), 1)
 
     def test_km_no_segregation(self):
-        self.assertAlmostEqual(si.km(data_a), 0)
+        self.assertAlmostEqual(si.km(data_b), 0)
 
 
 class TestCalcIndices(unittest.TestCase):
