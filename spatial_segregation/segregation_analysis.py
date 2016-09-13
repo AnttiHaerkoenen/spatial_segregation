8b54c912e2d5666ea8a82b54cf454fd9b8164320
AnttiHaerkonen <antth@hotmail.fi>
2016-09-13 15:18:31 +0300

    plotting function

--- spatial_segregation/segregation_analysis.py
+++ spatial_segregation/segregation_analysis.py
@@ -60,10 +60,10 @@ class SegregationAnalysis:
         if index not in self.indices:
             raise ValueError("Index not calculated")
 
-        self.simulations.plot.kde(legend="simulated {0}".format(index))
+        self.simulations.plot.kde()
         plt.axvline(self.indices[index], label="actual {0}".format(index))
         plt.legend()
-        plt.title('Simulated {0} index, n={1}'.format(index, len(self.simulations)))
+        plt.title('Simulated {0} index distribution, n={1}'.format(index, len(self.simulations)))
         plt.show()
 
 ########################################################################################################################
