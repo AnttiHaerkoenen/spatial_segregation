0761cfaaddf5b9699cf12000f503aec99a985297
AnttiHaerkonen <antth@hotmail.fi>
2016-10-18 17:09:22 +0300

    distance calculation

--- spatial_segregation/segregation_indices.py
+++ spatial_segregation/segregation_indices.py
@@ -12,19 +12,33 @@ def km(p_gn):
                                    np.nansum(p_gn, axis=1, keepdims=True))
 
     index = np.abs(p_gn / (p_g * p_n) - 1) * p_n * p_g
-    return np.nansum(index)
+    return round(np.nansum(index), 4)
 
 
 def mi(p_gn):
+    """
+    Spatial mutual information index.
+    :param p_gn: 2-dim array with groups as columns
+    :return: spatial mutual information index
+    """
     p_gn /= np.nansum(p_gn)
     p_g, p_n = np.broadcast_arrays(np.nansum(p_gn, axis=0, keepdims=True),
                                    np.nansum(p_gn, axis=1, keepdims=True))
 
     index = p_gn * np.log(p_gn / (p_g * p_n))
-    return np.nansum(index)
+    return round(np.nansum(index), 4)
 
 
 def calc_indices(p_gn):
+    """
+    Calculates Spatial forms of Karmel-MacLachlan index, Mutual information index,
+    :param p_gn: 2-dim array with groups as columns, or something that can be coerced with np.asarray
+    :return: dictionary of indices
+    """
+    try:
+        p_gn = np.asarray_chkfinite(p_gn)
+    except ValueError:
+        raise ValueError("Incorrect input. Remove NaNs and infs.")
 
     index = {
         'km': km(p_gn),
