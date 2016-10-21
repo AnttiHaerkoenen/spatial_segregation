8e82c72b045f3e6d5f93f6996fd93f92a1a045ff
AnttiHaerkonen <antth@hotmail.fi>
2016-10-21 14:53:24 +0300

    reformatted

--- spatial_segregation/segregation_indices.py
+++ spatial_segregation/segregation_indices.py
@@ -1,37 +1,38 @@
 import numpy as np
 
 
-def km(p_gn):
-    """
-    Karmel-MacLachlan index of segregation.
-    :param p_gn: 2-dim array with groups as columns
-    :return: km index
-    """
-    p_gn /= np.nansum(p_gn)
-    p_g, p_n = np.broadcast_arrays(np.nansum(p_gn, axis=0, keepdims=True),
-                                   np.nansum(p_gn, axis=1, keepdims=True))
+# def km(p_gn):
+#     """
+#     Karmel-MacLachlan index of segregation.
+#     :param p_gn: 2-dim array with groups as columns
+#     :return: km index
+#     """
+#     p_gn /= np.nansum(p_gn)
+#     p_g, p_n = np.broadcast_arrays(np.nansum(p_gn, axis=0, keepdims=True),
+#                                    np.nansum(p_gn, axis=1, keepdims=True))
+#
+#     index = np.abs(p_gn / (p_g * p_n) - 1) * p_n * p_g
+#     return round(np.nansum(index), 4)
+#
+#
+# def mi(p_gn):
+#     """
+#     Spatial mutual information index.
+#     :param p_gn: 2-dim array with groups as columns
+#     :return: spatial mutual information index
+#     """
+#     p_gn /= np.nansum(p_gn)
+#     p_g, p_n = np.broadcast_arrays(np.nansum(p_gn, axis=0, keepdims=True),
+#                                    np.nansum(p_gn, axis=1, keepdims=True))
+#
+#     index = p_gn * np.log(p_gn / (p_g * p_n))
+#     return round(np.nansum(index), 4)
 
-    index = np.abs(p_gn / (p_g * p_n) - 1) * p_n * p_g
-    return round(np.nansum(index), 4)
 
-
-def mi(p_gn):
-    """
-    Spatial mutual information index.
-    :param p_gn: 2-dim array with groups as columns
-    :return: spatial mutual information index
-    """
-    p_gn /= np.nansum(p_gn)
-    p_g, p_n = np.broadcast_arrays(np.nansum(p_gn, axis=0, keepdims=True),
-                                   np.nansum(p_gn, axis=1, keepdims=True))
-
-    index = p_gn * np.log(p_gn / (p_g * p_n))
-    return round(np.nansum(index), 4)
-
-
-def calc_indices(p_gn):
+def calc_indices(p_gn, indices='all'):
     """
     Calculates Spatial forms of Karmel-MacLachlan index, Mutual information index,
+    :param indices: which indices to calculate, default 'all'
     :param p_gn: 2-dim array with groups as columns, or something that can be coerced with np.asarray
     :return: dictionary of indices
     """
@@ -40,9 +41,21 @@ def calc_indices(p_gn):
     except ValueError:
         raise ValueError("Incorrect input. Remove NaNs and infs.")
 
-    index = {
-        'km': km(p_gn),
-        'mi': mi(p_gn)
-    }
+    if indices.lower() == 'all':
+        indices = ('km', 'mi')
+
+    index = {}
+
+    p_gn /= np.nansum(p_gn)
+    p_g, p_n = np.broadcast_arrays(np.nansum(p_gn, axis=0, keepdims=True),
+                                   np.nansum(p_gn, axis=1, keepdims=True))
+
+    if 'km' in indices:
+        km = np.abs(p_gn / (p_g * p_n) - 1) * p_n * p_g
+        index['km'] = round(km, 4)
+
+    if 'mi' in indices:
+        mi = p_gn * np.log(p_gn / (p_g * p_n))
+        index['mi'] = round(mi, 4)
 
     return index
