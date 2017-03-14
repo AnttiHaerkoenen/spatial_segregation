578a717afa71a80d36955ffaba41bac1ce1efd3c
AnttiHaerkonen <antth@hotmail.fi>
2017-03-14 17:20:24 +0200

    filling with zeros

--- src/data_utils.py
+++ src/data_utils.py
@@ -6,23 +6,37 @@ import numpy as np
 from src import data
 
 
-def fill_data_sheet(filename, sheet='Sheet1', columns='all'):
-    df = pd.read_excel(io=filename, sheetname=sheet)
-    if columns == 'all':
+def fill_data_sheet(input_input, output_file, sheet='Sheet1', columns='all', fill_with_zeros=None):
+    df = pd.read_excel(io=input_input, sheetname=sheet)
+
+    if not columns:
+        columns = pd.Index([])
+    elif columns == 'all':
         columns = df.columns
     elif isinstance(columns, str):
         columns = columns.split()
     else:
         columns = pd.Index(columns)
-        print(columns)
+
+    if not fill_with_zeros:
+        fill_with_zeros = pd.Index([])
+    elif fill_with_zeros == 'all':
+        fill_with_zeros = df.columns
+    elif fill_with_zeros == 'rest':
+        fill_with_zeros = pd.Index(set(df.columns) - set(columns))
+    elif isinstance(columns, str):
+        fill_with_zeros = fill_with_zeros.split()
+    else:
+        fill_with_zeros = pd.Index(fill_with_zeros)
 
     df[columns] = df[columns].fillna(method='ffill', axis=0)
+    df[fill_with_zeros] = df[fill_with_zeros].fillna(value=0)
 
-    df.to_excel(filename)
+    df.to_excel(output_file)
 
 
 if __name__ == '__main__':
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
     os.chdir(os.path.join(os.path.abspath(os.path.pardir), data.DATA_DIR))
 
-    fill_data_sheet('example.xlsx')
+    fill_data_sheet('example.xlsx', 'example2.xlsx', columns='district plot.number', fill_with_zeros='rest')
