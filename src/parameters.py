a20da7d501b7087ad91d8a3a950a7f6632b4f6bf
Antti Härkönen <antth@hotmail.fi>
2017-07-09 15:12:16 +0300

    parameters __add__ and __sub__

--- src/parameters.py
+++ src/parameters.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 
+from .exceptions import SSTypeError
+
 ParameterPermutation = namedtuple('ParameterPermutation', 'cell_size, kernel, bw, alpha')
 
 NEEDS_ALPHA = frozenset('biweight')
@@ -32,3 +34,21 @@ class Parameters:
     def __len__(self):
         return len(self.cell_sizes) * len(self.kernels) * len(self.bws) \
                * (1 + len(self.alphas) * len(self.alphas.intersection(NEEDS_ALPHA)))
+
+    def __add__(self, other):
+        if not other.instanceof(Parameters):
+            raise SSTypeError("{other} is not a {this}-class object!".format(other=other.__name__, this=self.__name__))
+
+        self.cell_sizes.update(other.cell_sizes)
+        self.kernels.update(other.kernels)
+        self.bws.update(other.bws)
+        self.alphas.update(other.alphas)
+
+    def __sub__(self, other):
+        if not other.instanceof(Parameters):
+            raise SSTypeError("{other} is not a {this}-class object!".format(other=other.__name__, this=self.__name__))
+
+        self.cell_sizes.difference_update(other.cell_sizes)
+        self.kernels.difference_update(other.kernels)
+        self.bws.difference_update(other.bws)
+        self.alphas.difference_update(other.alphas)
