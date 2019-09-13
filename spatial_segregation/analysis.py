b652f5cad83e97456b8bc995a0b1e68228419df7
antth <antth@hotmail.fi>
2019-09-13 15:06:28 +0300

    density

--- spatial_segregation/analysis.py
+++ spatial_segregation/analysis.py
@@ -1,10 +1,13 @@
 import os
 
+import numpy as np
 import pandas as pd
 import geopandas as gpd
 from segregation.spatial import SpatialMinMax
 from bokeh.plotting import figure, show, save
 from bokeh.models import GeoJSONDataSource
+import libpysal
+from libpysal.weights import Kernel
 
 from spatial_segregation.data import merge_dataframes, prepare_pop_data, prepare_point_data, aggregate_sum
 
@@ -13,9 +16,17 @@ def get_xy(
         geodf: gpd.GeoDataFrame,
         geometry_col: str = 'geometry',
 ) -> gpd.GeoDataFrame:
-    geodf['x'] = geodf[geometry_col].apply(lambda geom: tuple(geom.exterior.coords.xy[0]))
-    geodf['y'] = geodf[geometry_col].apply(lambda geom: tuple(geom.exterior.coords.xy[1]))
+    if geodf[geometry_col].geom_type[0] == 'Point':
+        geodf['x'] = geodf[geometry_col].apply(lambda geom: tuple(geom.coords.xy[0]))
+        geodf['y'] = geodf[geometry_col].apply(lambda geom: tuple(geom.coords.xy[1]))
+    else:
+        geodf['x'] = geodf[geometry_col].apply(lambda geom: tuple(geom.exterior.coords.xy[0]))
+        geodf['y'] = geodf[geometry_col].apply(lambda geom: tuple(geom.exterior.coords.xy[1]))
     return geodf
+
+
+def quartic(u, bandwidth):
+    return np.where(np.abs(u) <= bandwidth, bandwidth * 15/16 * (1 - u**2) ** 2, 0)
 
 
 def plot_density(
@@ -23,7 +34,9 @@ def plot_density(
         *,
         year,
         group,
+        kernel_function,
         crs=None,
+        **kwargs
 ):
     if crs is None:
         crs = {'init': 'epsg:3067'}
@@ -36,7 +49,16 @@ def plot_density(
     water_src = GeoJSONDataSource(geojson=water.to_json())
 
     pop = get_xy(data)
-    pop_src = GeoJSONDataSource(geojson=pop.to_json())
+    # pop_src = GeoJSONDataSource(geojson=pop.to_json())
+    minx, miny, maxx, maxy = pop['geometry'].total_bounds
+    x = np.arange(minx, maxx, 50)
+    y = np.arange(miny, maxy, 50)
+    X, Y = np.meshgrid(x, y)
+    xy = np.vstack([Y.ravel(), X.ravel()]).T
+    # todo XX YY
+    dist = np.sqrt(np.power(X, 2) + np.power(Y, 2))
+    density = kernel_function(dist, **kwargs)
+    print((XX**2 + YY**2)**0.5)
 
     fig.patches(
         xs='x',
@@ -47,8 +69,9 @@ def plot_density(
         line_color=None,
         line_width=0,
     )
+    fig.image(density)
 
-    show(fig)
+    # show(fig)
 
 
 if __name__ == '__main__':
@@ -70,4 +93,4 @@ if __name__ == '__main__':
         'OBJECTID', 'NUMBER', 'geometry',
         'other_christian', 'orthodox', 'other_religion', 'draft', 'lutheran',
     ]]
-    plot_density(data, group='orthodox', year=1880)
+    plot_density(data, group='orthodox', year=1880, kernel_function=quartic, bandwidth=100)
