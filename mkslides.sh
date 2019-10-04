1f839edba5e33205e4ce6e8f4d85c0f8dd876dbd
antth <antth@hotmail.fi>
2019-10-04 18:04:34 +0300

    slides without figures

--- mkslides.sh
+++ mkslides.sh
@@ -2,5 +2,5 @@
 
 cd slides
 pandoc -s --mathml -i -t dzslides slides-2019-10-07.md -o slides-2019-10-07.html
-sed -i 's/font-size: 80px/font-size: 60px/g' slides-2019-10-07.html
+sed -i 's/font-size: 80px/font-size: 55px/g' slides-2019-10-07.html
 cd ..
