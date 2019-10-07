a5cfb73dc4a6aba645a539b0264ebd3d471b9c55
antth <antth@hotmail.fi>
2019-10-07 13:16:53 +0300

    images

--- mkslides.sh
+++ mkslides.sh
@@ -3,7 +3,7 @@
 cd slides
 pandoc -s --mathml -i -t dzslides slides-2019-10-07.md -o slides-2019-10-07.html
 sed -i 's/font-size: 80px/font-size: 55px/g' slides-2019-10-07.html
-convert orthodox_1880.png -resize 50% orthodox_1880.jpg
+convert orthodox_1880.png -resize 10% orthodox_1880.jpg
 convert total_1880.png -resize 50% total_1880.jpg
 convert orthodox_1900.png -resize 50% orthodox_1900.jpg
 convert total_1900.png -resize 50% total_1900.jpg
