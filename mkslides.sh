6bbbb31f83910d1f7e895744c427a976f5183373
antth <antth@hotmail.fi>
2019-10-09 14:56:28 +0300

    hitu reveal.js

--- mkslides.sh
+++ mkslides.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd slides
-pandoc -s --mathml -i -t dzslides slides-hitu-2019.md -o slides-hitu-2019.html
+pandoc -s --mathml -i -t revealjs slides-hitu-2019.md -o slides-hitu-2019.html
 sed -i 's/font-size: 80px/font-size: 55px/g' slides-hitu-2019.html
 cd ..
