50e574d9e5bb58864123d525e8baf29a8bccc302
antth <antth@hotmail.fi>
2019-10-18 15:02:25 +0300

    heading sizes

--- mkslides.sh
+++ mkslides.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 cd slides
-pandoc -s --mathml -i -t revealjs slides-hitu-2019.md -o slides-hitu-2019.html
+pandoc -s --mathml -i -t revealjs slides-hitu-2019.md -o slides-hitu-2019.html --include-in-header custom.css
+sed 's/black.css/sky.css/g' slides-hitu-2019.html -i slides-hitu-2019.html
+sed 's///g' slides-hitu-2019.html -i slides-hitu-2019.html
 cd ..
