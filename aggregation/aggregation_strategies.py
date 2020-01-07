04598db592131244f15a65d1203991fa313cdb57
antth <antth@hotmail.fi>
2020-01-07 18:04:58 +0200

    combining

--- aggregation/aggregation_strategies.py
+++ aggregation/aggregation_strategies.py
@@ -5,15 +5,43 @@ import pandas as pd
 import geopandas as gpd
 
 from spatial_segregation.analysis import plot_density, kernel_density_surface
+from spatial_segregation.data import merge_dataframes, split_plots, aggregate_sum, prepare_pop_data
 
 
 if __name__ == '__main__':
     data_dir = Path('../data/')
-    points = gpd.read_file(data_dir / 'points1878.shp')
-    pop_by_plot = pd.read_excel(data_dir / 'pop_by_plot_1880.xlsx')
-    pop_by_page = pd.read_excel(data_dir / 'pop_by_page_1880.xlsx')
-    pop_by_district = pd.read_excel(data_dir / 'pop_by_district_1880.xlsx')
-    # todo combinations
 
-    plots_surface = kernel_density_surface()
+    district_codes = pd.read_csv(data_dir / 'district_codes.csv')
+    district_codes = {k: v for k, v in district_codes.itertuples(index=False)}
+
+    points = gpd.read_file(data_dir / 'plots_points_1878.shp')
+    points = split_plots(points, target_col='NUMBER')
+    points['district'] = [district_codes[int(d)] for d in points['DISTRICT']]
+    points['plot_number'] = [str(i) for i in points['NUMBER']]
+
+    pop_by_plot = pd.read_excel(data_dir / 'pop_by_plot_1880.xlsx')
+    pop_by_plot['plot_number'] = [str(n).split(',')[0] for n in pop_by_plot['plot_number']]
+
+    pop_by_page = pd.read_excel(data_dir / 'pop_by_page_1880.xlsx')
+
+    pop_by_district = pd.read_excel(data_dir / 'pop_by_district_1880.xlsx')
+
+
+    # todo combinations
+    plot_data = merge_dataframes(
+        location_data=points,
+        other_data=pop_by_plot,
+        on_location='district plot_number'.split(),
+        on_other='district plot_number'.split(),
+    )
+
+    print(plot_data['lutherans'])
+
+    # plots_surface = kernel_density_surface(
+    #     ,
+    #     group=,
+    #     bandwidth=,
+    #     cell_size=,
+    #     kernel_function=,
+    # )
     # todo kde + visualisation
--- spatial_segregation/convert_to_points.py
+++ spatial_segregation/convert_to_points.py
@@ -6,13 +6,15 @@ import geopandas as gpd
 
 def convert_to_points(
         data: gpd.GeoDataFrame,
+        crs: dict,
 ) -> gpd.GeoDataFrame:
     data.geometry = data.geometry.centroid
+    data = data.to_crs(crs)
     return data
 
 
 if __name__ == '__main__':
     data_dir = Path('../data')
     geodata = gpd.read_file(data_dir / 'plots_1878.shp')
-    geodata = convert_to_points(geodata)
+    geodata = convert_to_points(geodata, {'init': 'epsg:3067'})
     geodata.to_file(data_dir / 'plots_points_1878.shp')
